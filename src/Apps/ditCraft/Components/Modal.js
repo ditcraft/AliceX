d867a028b0cfeeafd8bce6eb0843a74f37128c1d
Yannik Goldgräbe <ykg@live.de>
2019-09-30 15:41:31 +0200

    Improved ditCraft dApp

--- src/Apps/ditCraft/Components/Modal.js
+++ src/Apps/ditCraft/Components/Modal.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import {
-  AsyncStorage,
   TouchableWithoutFeedback,
-  TouchableOpacity,
   StyleSheet,
   Text,
   Modal,
   View,
 } from 'react-native';
 import t from 'prop-types';
-import {Contract} from "../../../AliceSDK/Web3";
-import {ditCoordinatorABI} from "../ABI";
 
 
 class MyModal extends React.Component {
@@ -25,29 +21,6 @@ class MyModal extends React.Component {
   static defaultProps = {
     animationType: 'fade',
     transparent: true,
-  };
-
-  voteUp = async () => {
-    try {
-      //generate rand
-      await AsyncStorage.setItem('@ditCraft:key', 'I like to save it.');
-
-      const txHash = await Contract.write({
-        contractAddress: '0x9A65c773A216a4F4748B1D65C0fB039d9F2b223D',
-        abi: ditCoordinatorABI,
-        functionName: 'voteOnProposal',
-        parameters: ['', '', '']
-      });
-
-      console.log('txHash: ', txHash);
-
-    } catch(e) {
-      console.log('error: ', e)
-    }
-  };
-
-  voteDown = () => {
-
   };
 
   render() {
@@ -65,15 +38,6 @@ class MyModal extends React.Component {
         </TouchableWithoutFeedback>
         <View style={styles.modalContent}>
           {props.children}
-          <View style={{flexDirection: 'row',}}>
-            <TouchableOpacity onPress={this.voteUp} style={{backgroundColor: 'white', width: 100, height: 50, marginRight: 10}}>
-              <Text>Vote Up</Text>
-            </TouchableOpacity>
-            <TouchableOpacity onPress={this.voteDown} style={{backgroundColor: 'white', width: 100, height: 50}}>
-              <Text>Vote Down</Text>
-            </TouchableOpacity>
-
-          </View>
         </View>
         </Modal>
       </View>
@@ -84,9 +48,11 @@ const styles = StyleSheet.create({
   modalContent: {
     flex: 1,
     justifyContent: 'center',
-    margin: '5%',
+    margin: '7%',
     backgroundColor: '#2f3f6d',
-    color: 'white'
+    color: 'white',
+    borderRadius: 10,
+    marginTop: 35
   },
   modalOverlay: {
     position: 'absolute',
--- src/Apps/ditCraft/index.js
+++ src/Apps/ditCraft/index.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
-import { FlatList, StyleSheet, ActivityIndicator, Text, View, Dimensions, TouchableWithoutFeedback, Button } from 'react-native';
+import { FlatList, StyleSheet, ActivityIndicator, Text, View, Dimensions, AsyncStorage, TouchableOpacity, TouchableWithoutFeedback, Button, Image, ScrollView } from 'react-native';
 import ModalComponent from './Components/Modal.js';
-import Modalize from "./Components/Modalize.js";
 var sampledata = require('./Assets/sampledata.json');
 import env from '../../../env.json';
+import moment from 'moment';
+import {Contract} from "../../AliceSDK/Web3";
+import {ditCoordinatorABI} from "./ABI";
+/*import './shim.js'
+import {crypto} from 'crypto'
+import {ethers} from 'ethers'*/
 
 const { height, width } = Dimensions.get('window');
 
@@ -14,7 +19,7 @@ export default class ditCraft extends Component {
 			isLoading: true,
 			isModalVisible: false,
 			selectedItem: null,
-      dataSource: []
+      		dataSource: []
 		}
 	}
 
@@ -27,7 +32,8 @@ export default class ditCraft extends Component {
 			},
 			body: JSON.stringify({
 				api_key: env.ditcraft,
-				mode: 'demo'
+				mode: 'demo',
+				amount: 25
 			})
 		})
 		.then((response) => response.json())
@@ -55,6 +61,43 @@ export default class ditCraft extends Component {
 		this.showModal();
 	}
 
+	voteUp = async () => {
+		try {
+			//generate rand
+			// Create vote Hash
+			/*let choice = 0; // 0 = deny, 1 = accept
+			//let seed = crypto.randomBytes(32); // Needs to be stored! 
+			// (Maybe convert to string before storing since decimals would overflow. Maybe we ca use this: https://www.npmjs.com/package/biguint-format)
+
+			let voteHash = ethers.utils.solidityKeccak256([ 'uint256', 'uint256' ], [ choice, seed ]);
+
+			let repoHash = "0xc6bca7502e6d32e5354c70c5d918324ecaa19e2c93417c810d19fc28c7f2403f"
+
+			let proposalID = 0 // Depending on the proposal that we vote on
+
+			let numberOfKNW = 0 // We can test with 0 for now, we could retrieve the current free balance in the future and use that
+
+			// Function "voteOnProposal" needs to be called with these values (in that order): repoHash, proposalID, voteHash, numberOfKNW
+			*/
+			await AsyncStorage.setItem('@ditCraft:key', 'I like to save it.');
+		
+			const txHash = await Contract.write({
+				contractAddress: '0x9A65c773A216a4F4748B1D65C0fB039d9F2b223D',
+				abi: ditCoordinatorABI,
+				functionName: 'voteOnProposal',
+				parameters: ['', '', '']
+			});
+		
+			console.log('txHash: ', txHash);
+		} catch(e) {
+		  console.log('error: ', e)
+		}
+	};
+	
+	voteDown = () => {
+
+	};
+
 	render() {
 
 		if(this.state.isLoading){
@@ -67,30 +110,56 @@ export default class ditCraft extends Component {
 
 		let modalInfo;
 		if(this.state.selectedItem){
-			modalInfo = <View>
-					<Text>Topic: {this.state.selectedItem.topic}</Text>
-					<Text>ID: {this.state.selectedItem.id}</Text>
-					<Text>KNW VOTE ID: {this.state.selectedItem.knw_vote_id}</Text>
-					<Text>Label: {this.state.selectedItem.knw_label}</Text>
-					<Text>Repository: {this.state.selectedItem.repository}</Text>
+			modalInfo = <View style={{padding: 10}}>
+					<Text style={{color: '#fff'}}>
+						<Text style={{fontWeight: 'bold', color: '#fff'}}>[#{this.state.selectedItem.knw_vote_id}] </Text>
+						<Text>{this.state.selectedItem.topic}</Text>
+					</Text>
+					<Text style={{color: '#fff'}}>
+						<Text>Over at </Text>
+						<Text style={{fontWeight: 'bold'}}>{moment(this.state.selectedItem.reveal_phase_end).format('HH:mm:ss DD.MM.YYYY')}</Text>
+					</Text>
+					<View style={{flexDirection: 'row',}}>
+						<TouchableOpacity onPress={this.voteUp}>
+							<Image source={require('./Assets/Icons/arrow-up-bold-box-outline.png')}/>
+						</TouchableOpacity>
+						<TouchableOpacity onPress={this.voteDown} style={{position: 'absolute', right: 0}}>
+							<Image source={require('./Assets/Icons/arrow-down-bold-box-outline.png')}/>
+						</TouchableOpacity>
+					</View>
 				</View>
 		} else {
-			modalInfo = <Text>Test</Text>
+			modalInfo = <Text>No information</Text>
 		}
 		return (
 
 			<View style={styles.container}>
-				<FlatList
-					data={this.state.dataSource}
-					renderItem={({item}) =>
-					<TouchableWithoutFeedback onPress={ () => this.actionOnRow(item)}>
-						<Text style={styles.item}>{item.topic}</Text>
-					</TouchableWithoutFeedback>
-					}
-					/*keyExtractor={({id}, index) => id}*//>
+				<View style={styles.itemContainer}>
+					<Image style={styles.img}
+						source={require('./Assets/logo.png')}  
+						style={{width: 75, height: 75, borderRadius: 75/ 2}} 
+					/>
+				</View>
+				<ScrollView>
+					<FlatList
+						data={this.state.dataSource}
+						renderItem={({item}) =>
+						<View style={styles.item2}>
+							<TouchableWithoutFeedback onPress={ () => this.actionOnRow(item)}>
+								<Text style={styles.item}>
+									<Image source={item.accepted ? require('./Assets/Icons/check.png') : require('./Assets/Icons/close.png')} style={styles.buttonIcon}/>
+									<Text style={{color: '#2f3f6d', fontWeight: 'bold'}}>[#{item.knw_vote_id}] </Text>
+									<Text>{item.topic}</Text>
+								</Text>
+							</TouchableWithoutFeedback>
+						</View>
+
+						}
+						/*keyExtractor={({id}, index) => id}*//>
+				</ScrollView>
 				<ModalComponent
-				visible={this.state.isModalVisible}
-				dismiss={this.hideModal}>
+					visible={this.state.isModalVisible}
+					dismiss={this.hideModal}>
 					{modalInfo}
 				</ModalComponent>
 			</View>
@@ -101,14 +170,42 @@ export default class ditCraft extends Component {
 const styles = StyleSheet.create({
   container: {
    flex: 1,
-   paddingTop: 22
+   paddingTop: 22,
+   backgroundColor: '#f2f8f9'
   },
   item: {
     padding: 10,
     fontSize: 18,
-    height: 44,
+	height: 44,
+	marginBottom: 10,
+	justifyContent: 'center'
   },
   modal: {
     margin: 0,
+  },
+  itemContainer: {
+	justifyContent: 'center',
+	alignItems:'center',
+	height:130
+  },
+  img:{
+	flex: 1,
+	height: 100,
+	width: 100,
+	flexDirection: 'row',
+	alignItems: 'center',
+	justifyContent: 'center'
+  },
+  buttonIcon: {
+    resizeMode: 'contain',
+    width: 50,
+    height: 50
+  },
+  item2: {
+	backgroundColor: '#fff',
+	marginBottom: 10,
+	marginLeft: 10,
+	marginRight: 10,
+	borderRadius: 10
   }
 })
